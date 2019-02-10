a61f59182e6d3c3b138859ba356939b822b44acb
ofcRS <azeric054@gmail.com>
2019-02-10 22:07:46 +0400

    Initial commit

---
+++ src/containers/FlickSearcher/FlickSearcher.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class FlickSearcher extends Component {
+    render() {
+        return (
+            <div>FlickSearcher</div>
+        )
+    }
+}
+
+export default FlickSearcher;
